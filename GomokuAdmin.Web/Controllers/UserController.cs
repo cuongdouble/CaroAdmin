bcd005013aef2450713c4835e5ff711417d89baa
Tran Quoc Cuong <cuong934073@gmail.com>
2021-01-12 01:55:39 +0700

    create api controller

---
+++ GomokuAdmin.Web/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using GomokuAdmin.Web.Models;
+using GomokuAdmin.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace GomokuAdmin.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private UserService _userService { get; }
+
+        public UserController(UserService UserService)
+        {
+            _userService = UserService;
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult Search([FromQuery] string term = null)
+        {
+            return Json(_userService.Search(term));
+        }
+
+        //[HttpPatch("{id:int}")]
+        //public IActionResult BanChat(Guid userId)
+        //{
+        //    if (userId == null)
+        //        return BadRequest($"{nameof(userId)} is null.");
+        //    var result = _userService.BanChat(userId);
+        //    return Json(result);
+        //}
+
+    }
+}
