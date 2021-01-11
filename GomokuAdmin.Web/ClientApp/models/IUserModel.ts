4f51c62e772fe2c47fabfdc3d50a3c687491a44c
Tran Quoc Cuong <cuong934073@gmail.com>
2021-01-12 01:56:56 +0700

    add miss code module web client, add userModel

--- GomokuAdmin.Web/ClientApp/models/IUserModel.ts
+++ GomokuAdmin.Web/ClientApp/models/IUserModel.ts
@@ -6,11 +6,20 @@ export interface IUserModel {
     password: string;
     name: string;
     rank: number;
-    createAt: Date;
-    updateAt: Date;
+    createdAt: Date;
+    updatedAt: Date;
     numberOfMatches: number;
     numberOfWonMatches: number;
     firstName: string;
     lastName: string;
     photoUrl: string;
+    chatParticipants: any[];
+    chatRecords: any[];
+    friendParticipantUser1s: any[];
+    friendParticipantUser2s: any[];
+    friendRequestReceivers: any[];
+    friendRequestSenders: any[];
+    moveRecords: any[];
+    rankRecords: any[];
+    teamParticipants: any[];
 }
