23a1f7181596e2404591a575a0096076b7400a74
Tran Quoc Cuong <cuong934073@gmail.com>
2021-01-12 02:00:25 +0700

    create reducer

---
+++ GomokuAdmin.Web/ClientApp/store/index.ts
@@ -0,0 +1,38 @@
+import * as loginStore from "@Store/loginStore";
+import * as personStore from "@Store/personStore";
+import * as userStore from "@Store/userStore";
+import { connect } from "react-redux";
+
+// The top-level state object.
+export interface IApplicationState {
+    login: loginStore.ILoginStoreState;
+    person: personStore.IPersonStoreState;
+    user: userStore.IUserStoreState;
+}
+
+// Whenever an action is dispatched, Redux will update each top-level application state property using
+// the reducer with the matching name. It's important that the names match exactly, and that the reducer
+// acts on the corresponding ApplicationState property type.
+export const reducers = {
+    login: loginStore.reducer,
+    person: personStore.reducer,
+    user: userStore.reducer
+};
+
+// This type can be used as a hint on action creators so that its 'dispatch' and 'getState' params are
+// correctly typed to match your store.
+export interface IAppThunkAction<TAction> {
+    (dispatch: (action: TAction) => void, getState: () => IApplicationState): void;
+}
+
+export interface IAppThunkActionAsync<TAction, TResult> {
+    (dispatch: (action: TAction) => void, getState: () => IApplicationState) : Promise<TResult>
+}
+
+export function withStore<TStoreState, TActionCreators, TComponent extends React.ComponentType<TStoreState & TActionCreators & any>>(
+    component: TComponent,
+    stateSelector: (state: IApplicationState) => TStoreState,
+    actionCreators: TActionCreators
+): TComponent {
+    return <TComponent> <unknown>connect(stateSelector, actionCreators)(component);
+}
---
+++ GomokuAdmin.Web/ClientApp/store/userStore.ts
@@ -0,0 +1,48 @@
+﻿import { createSlice, PayloadAction, Dispatch } from '@reduxjs/toolkit';
+import UserService from '@Services/UserService';
+import { IUserModel } from '../models/IUserModel';
+
+// Declare an interface of the store's state.
+export interface IUserStoreState {
+    isFetching: boolean;
+    collection: IUserModel[];
+}
+
+// Create the slice.
+const slice = createSlice({
+    name: "user",
+    initialState: {
+        isFetching: false,
+        collection: []
+    } as IUserStoreState,
+    reducers: {
+        setFetching: (state, action: PayloadAction<boolean>) => {
+            state.isFetching = action.payload;
+        },
+        setData: (state, action: PayloadAction<IUserModel[]>) => {
+            state.collection = action.payload;
+        },
+    }
+});
+
+// Export reducer from the slice.
+export const { reducer } = slice;
+
+// Define action creators.
+export const actionCreators = {
+    search: (term?: string) => async (dispatch: Dispatch) => {
+        dispatch(slice.actions.setFetching(true));
+
+        const service = new UserService();
+
+        const result = await service.search(term);
+
+        if (!result.hasErrors) {
+            dispatch(slice.actions.setData(result.value));
+        }
+
+        dispatch(slice.actions.setFetching(false));
+
+        return result;
+    },
+};
